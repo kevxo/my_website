b22b62efd3c0fc90fb5378cb0707992865db683f
Kevin Cuadros <kev040798@gmail.com>
2021-03-15 14:06:34 -0400

    Create new folders like components and images. Add bootstrap to application

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import * as ReactBootStrap from 'react-bootstrap'
+import './card.css'
+
+class Card extends React.Component {
+  render() {
+    return (
+      <ReactBootStrap.Card className="card">
+        <ReactBootStrap.Card.Body>
+          <ReactBootStrap.Card.Title>About Me</ReactBootStrap.Card.Title>
+          <ReactBootStrap.Card.Text>
+            With supporting text below as a natural lead-in to additional content.
+          </ReactBootStrap.Card.Text>
+          <ReactBootStrap.Button variant="primary">Go somewhere</ReactBootStrap.Button>
+        </ReactBootStrap.Card.Body>
+      </ReactBootStrap.Card>
+    )
+  }
+}
+
+export default Card;
