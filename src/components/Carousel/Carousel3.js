220b3d1ebdc8a7fc7a91368d8c04a4f5d50e4726
Kevin Cuadros <kev040798@gmail.com>
2021-03-22 15:07:28 -0400

    Fix Carosuel alignment with their card.

--- src/components/Carousel/Carousel3.js
+++ src/components/Carousel/Carousel3.js
@@ -12,7 +12,7 @@ class Carousel3 extends React.Component {
           src="https://user-images.githubusercontent.com/63476564/101776107-72412000-3abe-11eb-840b-5ce7170ef0fe.png"
           alt="Second slide"
           height="10%"
-          width="14%"
+          width="15%"
         />
 
       </ReactBootStrap.Carousel.Item>
@@ -22,7 +22,7 @@ class Carousel3 extends React.Component {
         src="https://user-images.githubusercontent.com/63476564/101782315-d962d280-3ac6-11eb-83c8-5c84a7de2679.png"
         alt="Third slide"
         height="10%"
-          width="14%"
+          width="15%"
       />
     </ReactBootStrap.Carousel.Item>
     </ReactBootStrap.Carousel>
